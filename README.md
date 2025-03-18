01bf7f367a07bcd07d0deec4bb34a21b70b9a9f5
Sy Le <lenguyensy@gmail.com>
2025-03-17 20:01:58 -0700

    clean up keys

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ configs are in `.env.bak`. Update the content and rename the file as `.env`
 
 In Azure Active Directory, make sure you whitelist the following domain
 
-If domain is http://localhost:3000 (configurable as AAD_BASE_HOST_URL)
+If domain is http://localhost:3000 (configurable as AAD_SSO_BASE_HOST_URL)
 
 - http://localhost:3000/api/auth/login_callback
 
--- app/utils/backend/SSO.ts
+++ app/utils/backend/SSO.ts
@@ -1,11 +1,11 @@
 import { ConfidentialClientApplication } from "@azure/msal-node";
 
 // configs for SSO
-export const BASE_API_HOST = process.env.AAD_BASE_HOST_URL;
+export const BASE_API_HOST = process.env.AAD_SSO_BASE_HOST_URL;
 export const LOGIN_URL = "/api/auth/login";
 export const LOGIN_CALLBACK_URL = "/api/auth/login_callback";
 
-export const TENANT_ID = process.env["AAD_TENANT_ID"] || "common";
+export const TENANT_ID = process.env["AAD_SSO_TENANT_ID"] || "common";
 export const CLIENT_ID = process.env["AAD_SSO_CLIENT_ID"] || "";
 export const CLIENT_SECRET = process.env["AAD_SSO_CLIENT_VALUE"] || "";
 export const AUTHORITY = `https://login.microsoftonline.com/${TENANT_ID}`;
